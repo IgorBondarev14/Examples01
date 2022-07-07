c2d4ac200136e9b557520c51712e453e918a43a8
Igor <garkol@bk.ru>
2022-07-08 00:08:34 +0300

    add example 06 example

---
+++ example_06_base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 5;
+int c = 3;
+int d = 7;
+int e = 9;
+int max = a;
+
+if(b>max) max = b;
+if(c>max) max = c;
+if(d>max) max = d;
+if(e>max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
