6be1dd1d5668a6484ee8f8f82df6ccd52abd0ed6
Igor <garkol@bk.ru>
2022-07-08 00:00:58 +0300

    add Example 05 If/Else

---
+++ example_05_IFElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "igor")
+{
+    Console.WriteLine("Super. Nice to meet you, Igor!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
